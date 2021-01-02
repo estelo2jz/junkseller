e13ad55b077d2d61cd5edb8a6b4246c70d8ec7a0
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-01-02 13:58:53 -0600

    added items

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,71 +23,71 @@ function NavBar() {
                     <div className="sub-nav">
                         <div className="sub-nav-col left">
                             <Link to="/electronics" className="sub-nav-box" id="box-1">
-                                <div className="icon-background" id="icon-1">
+                                {/* <div className="icon-background" id="icon-1">
                                     <i className="fab fa-sellsy icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Electronics</h4>
                             </Link>
                             <Link to="/computers" className="sub-nav-box" id="box-2">
-                                <div className="icon-background" id="icon-2">
+                                {/* <div className="icon-background" id="icon-2">
                                     <i className="fas fa-user-lock icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Computers</h4>
                             </Link>
                             <Link to="/homegardentools" className="sub-nav-box" id="box-3">
-                                <div className="icon-background" id="icon-3">
+                                {/* <div className="icon-background" id="icon-3">
                                     <i className="fas fa-chart-line icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Home, Garden & Tools</h4>
                             </Link>
                             <Link to="/petsupplies" className="sub-nav-box" id="box-4">
-                                <div className="icon-background" id="icon-4">
+                                {/* <div className="icon-background" id="icon-4">
                                     <i className="fas fa-hand-holding-usd icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Pet Supplies</h4>
                             </Link>
                             <Link to="/outdoors" className="sub-nav-box" id="box-7">
-                                <div className="icon-background" id="icon-7">
+                                {/* <div className="icon-background" id="icon-7">
                                     <i className="fas fa-briefcase icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Outdoors</h4>
                             </Link>
                             <Link to="/automotiveindustrial" className="sub-nav-box" id="box-8">
-                                <div className="icon-background" id="icon-8">
+                                {/* <div className="icon-background" id="icon-8">
                                     <i className="fas fa-phone-alt icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Automotive & Industrial</h4>
                             </Link>
                         </div>
                         <div className="sub-nav-col right">
                             <Link to="/foodgrocery" className="sub-nav-box" id="box-5">
-                                <div className="icon-background" id="icon-5">
+                                {/* <div className="icon-background" id="icon-5">
                                     <i className="fas fa-trophy icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Food & Grocery</h4>
                             </Link>
                             <Link to="/beautyhealth" className="sub-nav-box" id="box-6">
-                                <div className="icon-background" id="icon-6">
+                                {/* <div className="icon-background" id="icon-6">
                                     <i className="far fa-calendar-check icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Beauty & Health</h4>
                             </Link>
                             <Link to="/toyskidsbaby" className="sub-nav-box" id="box-7">
-                                <div className="icon-background" id="icon-7">
+                                {/* <div className="icon-background" id="icon-7">
                                     <i className="fas fa-briefcase icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Toys, Kids & Baby</h4>
                             </Link>
                             <Link to="/handmade" className="sub-nav-box" id="box-8">
-                                <div className="icon-background" id="icon-8">
+                                {/* <div className="icon-background" id="icon-8">
                                     <i className="fas fa-phone-alt icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Handmade</h4>
                             </Link>
                             <Link to="/sports" className="sub-nav-box" id="box-6">
-                                <div className="icon-background" id="icon-6">
+                                {/* <div className="icon-background" id="icon-6">
                                     <i className="far fa-calendar-check icon"></i>
-                                </div>
+                                </div> */}
                                 <h4>Sports</h4>
                             </Link>
                         </div>
@@ -95,6 +95,8 @@ function NavBar() {
                 </li>
                 </ul>
             </nav>
+        </div>
+        <div>
           <Link to="/checkout" className="header__link">
             <div className="header__optionBasket">
               <ShoppingBasketIcon />
--- src/components/category/Electronics.js
+++ src/components/category/Electronics.js
@@ -13,166 +13,166 @@ function Electronics() {
           <div className="electronics__container-flex-1">
               <Product 
                 id = "123123"
-                title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                price = {599.99}
+                title = "LG 34WN80C-B 34 inch 21:9 Curved UltraWide WQHD IPS Monitor with USB Type-C Connectivity sRGB 99% Color Gamut and HDR10 Compatibility, Black (2019)"
+                price = {549.99}
                 rating = {5}
-                image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                image = "https://images-na.ssl-images-amazon.com/images/I/81WBbFOEHwL._AC_SL1500_.jpg"
               />
               <Product 
                 id = "123123"
-                title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                price = {599.99}
+                title = "Acer Predator Triton 500 PT515-52-73L3 Gaming Laptop, Intel i7-10750H, NVIDIA GeForce RTX 2070 SUPER, 15.6' FHD NVIDIA G-SYNC Display, 300Hz, 16GB Dual-Channel DDR4, 512GB NVMe SSD, RGB Backlit KB"
+                price = {1659.99}
                 rating = {5}
-                image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                image = "https://images-na.ssl-images-amazon.com/images/I/71HWQq7dX2L._AC_SL1500_.jpg"
               />          
               <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.99}
+                  title = "ASUS ROG Rapture GT-AX11000 AX11000 Tri-Band 10 Gigabit WiFi 6 Gaming Router, 2.5 Gb LAN port, AiProtection Lifetime Internet Security, Aimesh Compatible, Easy App setup, 802.11Ax"
+                  price = {449.99}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://images-na.ssl-images-amazon.com/images/I/71zLTz2duHL._AC_SL1500_.jpg"
               />          
               <Product 
                 id = "123123"
-                title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                price = {599.99}
+                title = "New Apple iPad Pro (12.9-inch, Wi-Fi, 1TB) - Silver (4th Generation)"
+                price = {1449.99}
                 rating = {5}
-                image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                image = "https://images-na.ssl-images-amazon.com/images/I/81FH2j7EnJL._AC_SL1500_.jpg"
               />
               <Product 
                 id = "123123"
-                title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                price = {599.99}
-                rating = {5}
-                image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                title = "ColoWorld Remanufactured 63XL Ink Cartridge Combo Pack Replacement for HP 63XL 63 XL Work with HP Officejet 5255 5258 3830 3834 Envy 4520 4153 4650 Deskjet 1112 3639 3636 Printer (1 Black 1 Tri-Color)"
+                price = {39.65}
+                rating = {4}
+                image = "https://images-na.ssl-images-amazon.com/images/I/71XdYh3D07L._AC_SL1500_.jpg"
               />          
               <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.99}
+                  title = "iBUYPOWER Gaming PC Computer Desktop Element MR 9320 (Intel i7-10700F 2.9GHz, NVIDIA GTX 1660 Ti 6GB, 16GB DDR4 RAM, 240GB SSD, 1TB HDD, Wi-Fi ready, Windows 10 Home)"
+                  price = {1352.99}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://images-na.ssl-images-amazon.com/images/I/715zrA5cmLL._AC_SL1500_.jpg"
               />   
           </div>
           <div className="electronics__container-3-grid">
             <div className="electronics__container-3-grid-grid-1">
               <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.991}
+                  title = "AMD Ryzen 9 3900X 12-core, 24-thread unlocked desktop processor with Wraith Prism LED Cooler"
+                  price = {546.88}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://images-na.ssl-images-amazon.com/images/I/71S31CWSs3L._AC_SL1093_.jpg"
                 />
                 <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.991}
+                  title = "Klipsch Synergy Black Label B-100 Bookshelf Speakers"
+                  price = {699.99}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://images-na.ssl-images-amazon.com/images/I/51w32vF7xgL._AC_SL1143_.jpg"
                 />          
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.991}
-                    rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    title = "USB 3.0 Universal Laptop Docking Station, Acodot Dual Monitor Dock for Windows Equipped with Dual HDMI and DVI/VGA, 6 USB 3.0 Ports, Gigabit Ethernet, Audio"
+                    price = {64.58}
+                    rating = {4}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/616Vux27VAL._AC_SL1500_.jpg"
                 />  
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.991}
-                    rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    title = "Dell 452-BCYT D6000 Universal Dock, Black, Single"
+                    price = {163.71}
+                    rating = {3}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71E9yyx0pbL._AC_SL1500_.jpg"
                 />  
             </div>
             <div className="electronics__container-3-grid-grid-2">
               <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.992}
-                  rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  title = "CyberPower CP1500PFCLCD PFC Sinewave UPS System, 1500VA/1000W, 12 Outlets, AVR, Mini Tower Black"
+                  price = {209.95}
+                  rating = {4}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/71mhftPO1dL._AC_SL1500_.jpg"
                 />
                 <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.992}
-                  rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  title = "Echo Show 5 -- Smart display with Alexa – stay connected with video calling - Charcoal"
+                  price = {49.99}
+                  rating = {4}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/61G86qzw%2BwL._AC_SL1000_.jpg"
                 />          
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.992}
+                    title = "Yamaha NS-AW150BL 2-Way Indoor/Outdoor Speakers (Pair, Black) - Wired"
+                    price = {399.99}
                     rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    image = "https://images-na.ssl-images-amazon.com/images/I/81rj65QqF0L._AC_SL1500_.jpg"
                 />  
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.991}
+                    title = "ASUS UX534FTC-AS77 ZenBook 15 Laptop, 15.6” UHD 4K NanoEdge Display, Intel Core i7-10510U, GeForce GTX 1650, 16GB, 512GB PCIe SSD, ScreenPad 2.0, Amazon Alexa Compatible, Windows 10, Icicle Silver"
+                    price = {1148.58}
                     rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    image = "https://images-na.ssl-images-amazon.com/images/I/81W7WT%2BOmsL._AC_SL1500_.jpg"
                 />  
             </div>
             <div className="electronics__container-3-grid-grid-3">
               <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.993}
+                  title = "Apple Magic Keyboard for iPad Air (4th Generation) and iPad Pro 11-inch - US English"
+                  price = {289.99}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://images-na.ssl-images-amazon.com/images/I/71fYV0JMoYL._AC_SL1500_.jpg"
                 />
                 <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.993}
+                  title = "New Apple iPhone 12 Pro Max (128GB, Pacific Blue) [Locked] + Carrier Subscription"
+                  price = {1099.99}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://m.media-amazon.com/images/I/71MHTD3uL4L._FMwebp__.jpg"
                 />          
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.993}
-                    rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    title = "Sony WH-1000XM4 Wireless Industry Leading Noise Canceling Overhead Headphones with Mic for Phone-Call and Alexa Voice Control, Black"
+                    price = {278.00}
+                    rating = {4}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg"
                 />  
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.991}
-                    rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    title = "HP LaserJet Pro M29w Wireless All-in-One Laser Printer, Works with Alexa (Y5S53A)"
+                    price = {299.99}
+                    rating = {3}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/71N7y7DHrCL._AC_SL1500_.jpg"
                 />  
             </div>
             <div className="electronics__container-3-grid-grid-3">
               <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.993}
-                  rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  title = "Fire TV Cube | Hands-free streaming device with Alexa | 4K Ultra HD | 2019 release"
+                  price = {19.99}
+                  rating = {4}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/61Zdp5vTn4L._AC_SL1000_.jpg"
                 />
                 <Product 
                   id = "123123"
-                  title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                  price = {599.993}
+                  title = "TaoTronics Projector Screen with Stand, TT-HP020 Indoor Outdoor Movie Projection Screen 4K HD with Wrinkle-Free Design (Easy to Clean, 1.1Gain, 160° Viewing Angle & Includes A Carry Bag)"
+                  price = {99.99}
                   rating = {5}
-                  image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                  image = "https://images-na.ssl-images-amazon.com/images/I/71hOdTAlxQL._AC_SL1500_.jpg"
                 />          
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.993}
-                    rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    title = "1080P Projector - Artlii Energon 2 Full HD WiFi Bluetooth Projector Support 4K, 7000L 300' Display, Compatible with HDMI, iPhone, Android for Home Theater, PPT Presentation"
+                    price = {229.49}
+                    rating = {4}
+                    image = "https://images-na.ssl-images-amazon.com/images/I/713DuHr6LJL._AC_SL1500_.jpg"
                 />  
                 <Product 
                     id = "123123"
-                    title = "Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W"
-                    price = {599.991}
+                    title = "LG 50UN7300PUF Alexa BuiltIn 50Inch 4K Ultra HD Smart LED TV 2020"
+                    price = {396.99}
                     rating = {5}
-                    image = "https://m.media-amazon.com/images/I/71Q5sdPHD-L._AC_UY218_.jpg"
+                    image = "https://images-na.ssl-images-amazon.com/images/I/81RMDc4CWYL._AC_SL1500_.jpg"
                 />  
             </div>
           </div>
