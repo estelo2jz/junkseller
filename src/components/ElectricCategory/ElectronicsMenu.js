abbae852f67a683db000b4b7facb02c10f36b0db
esteloabell <manitobaaa@gamil.com>
2021-01-04 19:02:02 -0600

    added new chnages on styles, trying to add active style on click

--- src/components/ElectricCategory/ElectronicsMenu.js
+++ src/components/ElectricCategory/ElectronicsMenu.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import $ from "jquery";
 import './ElectronicsMenu.scss';
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
 function ElectronicsMenu(props) {
+  $(document).on('click', 'ul li', function () {
+    $(this).addClass('active').siblings().removeClass('active')
+  })
   return (
       <div className="electronics-menu">
         <div className="electronics-menu-items">
-          <Link to="/tvvideo">
+          <Link class="active" to="/tvvideo">
             <span>
               {props.category0}
             </span>
