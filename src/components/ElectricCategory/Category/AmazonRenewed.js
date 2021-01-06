543b60351610acc97de969f77ac433eca370ace1
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-01-06 14:32:51 -0600

    fixed style on electronics component

--- src/components/ElectricCategory/Category/AmazonRenewed.js
+++ src/components/ElectricCategory/Category/AmazonRenewed.js
@@ -10,7 +10,7 @@ function AmazonRenewed() {
       </div>
       <div className="amazon-renewed__header">
         <div className="amazon-renewed__heading">
-          <h1>Renewed Electronics</h1>
+          <h1>Renewed Category</h1>
         </div>
         <div className="amazon-renewed__category-container">
           <div className="amazon-renewed__image-container">
@@ -38,114 +38,117 @@ function AmazonRenewed() {
             <img src="https://images-na.ssl-images-amazon.com/images/G/01/us-certifiedrefurbished/UK_Amazon-Renewed-1052644_20-6-2017_670x700-v8._CB507073535_.jpg" alt="renew-category" />
           </div>
         </div>
+        <div className="amazon-renewed__heading">
+          <h1>Renewed Electronics</h1>
+        </div>
       </div>
       <div className="amazon-renewed__grid-flex">
         <div className="amazon-renewed__grid-item">
           <ElectronicsProductTemplate 
             id = "123123"
-            title = ""
-            price = {29.99}
+            title = "Apple iPhone XR, 64GB, Black - Fully Unlocked (Renewed)"
             rating = {4}
-            image = ""
+            price = {363.00}
+            image = "https://images-na.ssl-images-amazon.com/images/I/414KiPpTsXL.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "Apple iPad Air 2, 64 GB, Space Gray, (Renewed)"
+            rating = {4}
+            price = {265.20}
+            image = "https://images-na.ssl-images-amazon.com/images/I/51eSDQm377L.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "Microsoft Xbox One Kinect Sensor Bar [Xbox One](Renewed)"
+            rating = {4}
+            price = {199.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41Ry7kwKYxL.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "Lenovo ThinkCentre M93P Tiny Mini Business Desktop Computer, Intel Dual-Core i5-4570T Processor up to 3.60 GHz, 8GB RAM, 240GB SSD, WiFi, Windows 10 Pro (Renewed)"
+            rating = {4}
+            price = {197.98}
+            image = "https://images-na.ssl-images-amazon.com/images/I/410+Mn+qtqL.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "JLAB Audio Jbuds Air True Wireless Signature Bluetooth Earbuds, Charging Case, Black, IP55 Sweat Resistance, Bluetooth 5.0 Connection (Renewed)"
+            rating = {4}
+            price = {29.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/311EleaXm+L.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "Beats by Dr dre Powerbeats2 Wireless In-Ear Bluetooth Headphone with Mic - Black (Renewed)"
+            rating = {4}
+            price = {69.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41ZbmWBjD9L.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "GoPro HERO8 Black 4K Waterproof Action Camera - Black (Renewed)"
+            rating = {4}
+            price = {249.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/31QPKAoxnpL.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "Wii Console w/ Bonus Wii Sports Resort & Wii MotionPlus Bundle (Renewed)"
+            rating = {4}
+            price = {244.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41i1R6g4GJL.jpg"
+          />
+        </div>
+        <div className="amazon-renewed__grid-item">
+          <ElectronicsProductTemplate 
+            id = "123123"
+            title = "Dell OptiPlex 7050 Small Form Factor | Intel Core i5-6600 | 8 GB DDR4 | 500 GB HDD | Wi-Fi | Windows 10 Pro (Renewed)"
+            rating = {3}
+            price = {459.00}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41sS0aziJkL.jpg"
           />
         </div>
         <div className="amazon-renewed__grid-item">
           <ElectronicsProductTemplate 
             id = "123123"
             title = ""
-            price = {29.99}
             rating = {4}
-            image = ""
+            price = {295.00}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41f5DkSgNPL.jpg"
           />
         </div>
         <div className="amazon-renewed__grid-item">
           <ElectronicsProductTemplate 
             id = "123123"
             title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
+            rating = {3}
+            price = {209.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41HxEQF35pL.jpg"
           />
         </div>
         <div className="amazon-renewed__grid-item">
           <ElectronicsProductTemplate 
             id = "123123"
             title = ""
-            price = {29.99}
             rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
-          />
-        </div>
-        <div className="amazon-renewed__grid-item">
-          <ElectronicsProductTemplate 
-            id = "123123"
-            title = ""
-            price = {29.99}
-            rating = {4}
-            image = ""
+            price = {239.99}
+            image = "https://images-na.ssl-images-amazon.com/images/I/41+gQ3SfqoL.jpg"
           />
         </div>
       </div>
--- src/components/ElectricCategory/Category/HomeAudioTheater.js
+++ src/components/ElectricCategory/Category/HomeAudioTheater.js
@@ -6,79 +6,78 @@ import '../ElectronicStyles/HomeAudioTheater.scss';
 function HomeAudioTheater() {
   return (
     <div className="home-audio-theater__container">
-      <div>
-        <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Consumer_Electronics/XCM_Manual_1301610_1538042_US_GD21_us_ce_storefront_stripes_2362205_us_en_3625898_1500x90_en_US.jpg" alt="ads-image" />
+      <div className="home-audio-theater__ads-one">
         <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Home_Entertainment/XCM_Manual_1296378_1515069_US_us_home_entertainment_categorystorefront_us_en_3593549_3000x600_en_US.jpg" alt="ads-image" />
       </div>
       <div className="home-audio-theater__heading">
-        <h1>Home Audio Theater</h1>
+        <h2>Home Audio Theater</h2>
       </div>
       <div className="home-audio-theater__category">
-      <div className="home-audio-theater__heading">
-        <h1>Shop by Category</h1>
-      </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__heading">
+          <h3>Shop by Category</h3>
+        </div>
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_wirelessaudio.jpg" alt="image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_premiumaudio.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_hometheater.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_stereocomponents.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-voice-service/category_tile/AVS_HomeAudioTile_AlexaBuiltIn_Capital_Polk._CB485008781_.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_headphones.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_speakers.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_turntables.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_audioaccessories.jpg" alt="image"/>
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/cat-tile/ce_aa_cat-tile_desktop_deals.jpg" alt="image"/>
         </div>
       </div>
-      <div>
-        <div>
+      <div className="home-audio-theater__ads-two-container">
+        <div className="home-audio-theater__ads-two">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Consumer_Electronics/XCM_Manual_1299910_1529403_US_us_ce_storefront_3629787_1500x440_en_US.jpg" alt="ads-image"/>
         </div>
       </div>
       <div className="home-audio-theater__heading">
         <h1>Shop by Brand</h1>
       </div>
-      <div className="home-audio-theater__category">
-        <div className="tv-video__flex-box">
+      <div className="home-audio-theater__brand-category">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/brand-tile/ce_aa_brand-tile_desktop_bose.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/brand-tile/ce_aa_brand-tile_desktop_sony.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/PC_Hardware/SonosLogoFarm1.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/brand-tile/ce_aa_brand-tile_desktop_yamaha.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/brand-tile/ce_aa_brand-tile_desktop_marshall.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/brand-tile/ce_aa_brand-tile_desktop_klipsch.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/PC_Hardware/samsunglogoUSE.jpg" alt="brand-image" />
         </div>
-        <div className="tv-video__flex-box">
+        <div className="home-audio-theater__flex-box">
           <img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2018/img/Consumer_Electronics/aa/brand-tile/ce_aa_brand-tile_desktop_amznbasics.jpg" alt="brand-image" />
         </div>
       </div>
