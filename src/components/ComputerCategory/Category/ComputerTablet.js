aa66d4d8b203c07410c796d962c7207ff39712bf
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-01-20 09:15:03 -0600

    styles

--- src/components/ComputerCategory/Category/ComputerTablet.js
+++ src/components/ComputerCategory/Category/ComputerTablet.js
@@ -6,16 +6,28 @@ function ComputerTablet() {
     <div className="computer-tablet__container">
       <div className="computer-tablet__box-one-container">
         <div className="computer-tablet__box-one-one">
-          <span>1</span>
+          <div className="computer-tablet__box-one-img">
+            <img className="responsive" src="https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL._AC_SX184_.jpg" alt="product-image" />
+          </div>
+          <span>Jackets & coats</span>
         </div>
         <div className="computer-tablet__box-one-two">
-          <span>2</span>
+          <div className="computer-tablet__box-one-img">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/41jSuUHT8eL._AC_SX184_.jpg" alt="product-image" />
+          </div>
+          <span>Watches</span>
         </div>
         <div className="computer-tablet__box-one-three">
-          <span>3</span>
+          <div className="computer-tablet__box-one-img">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51rGP3HU3HL._AC_SX184_.jpg" alt="product-image" />
+          </div>
+          <span>Sunglasses</span>
         </div>
         <div className="computer-tablet__box-one-four">
-          <span>4</span>
+          <div className="computer-tablet__box-one-img">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/41Qf7+v71YL._AC_SX184_.jpg" alt="product-image" />
+          </div>
+          <span>Sweaters</span>
         </div>
       </div>
     </div>
