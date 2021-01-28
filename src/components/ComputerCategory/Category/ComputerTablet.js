57625f7e63b362a9c2a8ec1e80e97aa53b48daa7
esteloabell <manitobaaa@gamil.com>
2021-01-27 18:00:12 -0600

    added dummy products

--- src/components/ComputerCategory/Category/ComputerTablet.js
+++ src/components/ComputerCategory/Category/ComputerTablet.js
@@ -64,21 +64,19 @@ function ComputerTabletTemplate({ id, title, image, price, rating, rateNumber, s
   )
 }
 
-function Upccoming() {
+function Upccoming({upImage, upTitle, upTime}) {
   return (
     <div className="upcoming__container">
       <div className="upcoming__top">
         <div className="upcoming__img">
-          <img src="https://images-na.ssl-images-amazon.com/images/I/71gEjv5GXJL._AC_SL1500_.jpg" alt="upcoming-img" />
+          <img src={upImage} alt="upcoming-img" />
         </div>
         <p>early access deal</p>
       </div>
       <div className="upcoming__bottom">
         <div className="upcoming__info">
-          <p>
-            Full HD 1080P AutoFocus Webcam with Microphones & Privacy Cover,USB Video Web Cam for Computers PC Laptop Desktop,Web Camera for Live Streaming,Conference Study Video Calling and Chatting,Skype
-          </p>
-          <p>Starts for you in 48:30</p>
+          <p>{upTitle}</p>
+          <p>Starts for you at {upTime}</p>
           <p>Get access to this deal 30 minutes earlier with <span>Member Access</span>.</p>
         </div>
       </div>
@@ -268,22 +266,126 @@ function ComputerTablet() {
             </div>
             <div className="tabs-template__outer-container">
               <div className="tabs-template__upcoming">
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
-                <Upccoming />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/71VO3hIy7ZL._AC_SL1500_.jpg"
+                  upTitle= "Laptop Backpack with USB Charging Port,Slim Travel Backpack with Laptop Compartment for Men and Women,Water Resistant College School BookBag Computer Bag for Girls and Boys Fits 15.6 In Laptop, Blue"
+                  upTime = "7:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/71syj0gvm6L._SL1500_.jpg"
+                  upTitle = "Cat8 Ethernet Cable 25 ft, High Speed Outdoor Indoor & in Wall LAN Internet Flat Cable, Weatherproof&UV Resistant, 40Gbps 2000Mhz Solid Cat 8 Heavy"
+                  upTime = "9:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/91m0qe0%2BsQL._AC_SL1500_.jpg"
+                  upTitle= "Voova 11.6-12 Inch Laptop Sleeve Case Cover, Water Resistant Computer Protective Bag Compatible with MacBook Air 11, Mac 12,Surface Pro X 7 6 5 4,Samsung Asus Acer Chromebook 3 4 R11 with Handle,Khaki"
+                  upTime = "10:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/616jZEMvxVL._AC_SL1200_.jpg"
+                  upTitle= "Mini PC Beelink U57 Intel Core i5-5257u Processor(up to 3.10 GHz) Windows 10 Pro,8G DDR3/512G SSD High Performance Business Mini Computer,4K UHD,2.4G/5G Dual WiFi,BT4.2,Dual HDMI Ports"
+                  upTime = "7:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/61Nqnyk2XEL._AC_SL1500_.jpg"
+                  upTitle= "Golvery Wireless Headphones for TV Watching Listening w/Transmitter Adapter, Rechargeable Over Ear Bluetooth Headset Stereo Hearing Set Ideal for Seniors, Plug n Play, No Audio Delay, 15H Playtime"
+                  upTime = "8:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/71j2DPXfHaL._AC_SL1500_.jpg"
+                  upTitle= "KRISLOG for iPhone Charger, MFi Certified Lightning Cable 10Ft 6Ft×2 3Ft×2 Braided iPhone Cable Data Sync Transfer Cord Compatible with iPhone 11 Pro Max/XS MAX/XR/XS/X/8/7/Plus/6S 5-Pack Updated"
+                  upTime = "10:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/51VSlutsg9L._AC_SL1080_.jpg"
+                  upTitle= "livho 2 Pack Blue Light Blocking Glasses, Computer Reading/Gaming/TV/Phones Glasses for Women Men,Anti Eyestrain & UV Glare (Light Blcak+Clear)"
+                  upTime = "12:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/71eUB2EqoML._AC_SL1500_.jpg"
+                  upTitle= "ACEPC T11 Mini PC Windows 10 Pro, 8GB DDR3/128GB eMMC Fanless Mini Computer Intel Atom Z8350 Desktop Computer Support Dual Display, 4K HD, Bluetooth 4.2, Gigabit LAN, Dual Band WiFi"
+                  upTime = "06:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/61FotETvF1L._AC_SL1500_.jpg"
+                  upTitle= "Stylus Pen for iPad Compatible with Apple Pencil 2 Generation for iPad/Pro 2018/2019/2020 with Palm Rejection White"
+                  upTime = "02:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/61Q1ciCSyjL._AC_SL1000_.jpg"
+                  upTitle = "Mirabox USB3.0 HDMI Video Capture Card, 1080P 60FPS HD Game Capture Device Cam Link with HDMI Passthrough Work with DSLR Xbox PS4 Nintendo Switch for OBS"
+                  upTime = "06:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/614uA6BT8hL._AC_SL1200_.jpg"
+                  upTitle= "Beelink GTR Mini PC Windows 10 Pro,AMD Ryzen 7 3750H Processor(up to 4GHz) 8GB DDR4 256GB NVMe SSD,Radeon Vega 8 Graphics,WiFi 6,Bluetooth 5.0,Dual Gigabit"
+                  upTime = "08:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/81EaFVru57L._AC_SL1500_.jpg"
+                  upTitle= "USB C Cable/Type C to USB Fast Charging Cord [Pack of 5], Gold-Plated & Nylon Braided (10ft + 6.5ft + 2x3.3ft + 1ft) for Samsung Galaxy S8/Plus, Nexus"
+                  upTime = "07:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/71awKlTeZWL._SL1500_.jpg"
+                  upTitle = "ABKONCORE HR120 RGB Fans, 3 Pack 120mm Computer Fans, 5V ARGB Motherboard SYNC/Remote Controller, Ajustable Fan Speed and Color, Hydro Bearing Case Fan"
+                  upTime = "06:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/615a-okh1rL._AC_SL1500_.jpg"
+                  upTitle= "Webcam with Microphone for Desktop, DEPSTECH 2K QHD USB Web Cam with Auto Light Correction, Desktop Computer Camera Streaming Camera for Video Conferencing"
+                  upTime = "08:00 am"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/61CdiF59R1L._AC_SL1200_.jpg"
+                  upTitle = "Travel Laptop Backpack,Extra Large Anti Theft College School Backpack for Men and Women with USB Charging Port,Water Resistant Big Business Computer"
+                  upTime = "12:00 pm"
+                />
+                <Upccoming
+                  upImage= "https://images-na.ssl-images-amazon.com/images/I/71QNSGP13DL._AC_SL1280_.jpg"
+                  upTitle = "Portable Monitor - Lepow Upgraded 15.6 Inch 1920 x 1080 Full HD IPS Eye Care Second Screen Ultra-Slim Computer Game Display with Mini HDMI Type-C Port"
+                  upTime = "06:00 am"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/61bgaGVfevL._AC_SL1500_.jpg"
+                  upTitle="USB Docking Station,UOEOS 13 in 1 Triple Display 3.0 USB C Laptop Docking Station Dual Monitor with HDMI & DVI/VGA 5 USB 3.0 Ports,Gigabit Ethernet"
+                  upTime="09:00 am"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/61qwyYns9UL._AC_SL1200_.jpg"
+                  upTitle="Cordless Air Duster, KEYNICE 2-in-1 Dust Cleaner and Vacuum, 6000mAh Battery, 40W Fast Charging, Powerful 36000 RPM, Electric Air Blower for Cleaning"
+                  upTime="03:00 pm"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/71ZQU-BAACL._AC_SL1500_.jpg"
+                  upTitle="Kids Tablet 7 Android Kids Tablet Toddler Tablet Kids Edition Tablet with WiFi Dual Camera Childrens Tablet 1GB + 16GB Parental Control, Google Play Store"
+                  upTime="12:00 pm"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/71vrvhtKAuL._AC_SL1500_.jpg"
+                  upTitle="Laptop Stand,Laptop Holder Riser Computer Stand,Aluminum 7Angles Adjustable Ergonomic Non-Slip Ventilated Desktop Holder, Compatible with MacBook Air Pro"
+                  upTime="05:00 am"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/61m8hZUKfRL._AC_SL1500_.jpg"
+                  upTitle="TeckNet 2.4G Rechargeable Wireless Vertical Ergonomic Optical Mouse with USB Nano Receiver, 800 / 1200 /1600 /2000 /2400DPI, 5 Buttons for Laptop, MacBook"
+                  upTime="09:00 pm"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/61VdXnSWYOL._AC_SL1500_.jpg"
+                  upTitle="S40 Mini PC Stick Intel Celeron N4000(up to 2.6GHz) Windows 10 Pro Mini Computer Stick, DDR4 4GB+64GB eMMC, UHD 4K@60Hz, Dual Band WiFi, HDMI&Mini DP"
+                  upTime="02:00 am"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/61C9FoLzjeL._AC_SL1200_.jpg"
+                  upTitle="USB C to USB Adapter, JSAUX [0.5ft 2 Pack] Type C 3.0 OTG Cable On The Go Type C Male to USB A Female Adapter Compatible with MacBook Pro 2018 2017,Samsung"
+                  upTime="04:00 am"
+                />
+                <Upccoming
+                  upImage="https://images-na.ssl-images-amazon.com/images/I/61bpR2XdcWL._AC_SL1000_.jpg"
+                  upTitle="BV-Tech 6 Port PoE+ Switch (4 PoE+ Ports with 2 Ethernet Uplink and Extend Function) – 60W – 802.3at + 1 High Power PoE Port| Desktop Fanless"
+                  upTime="08:00 pm"
+                />
               </div>
             </div>
             <div className="tabs-template__outer-container">
@@ -293,7 +395,6 @@ function ComputerTablet() {
             </div>
             <div className="tabs-template__outer-container">
               <div className="tabs-template__container">
-                <Upccoming />
               </div>
             </div>
           </div>
