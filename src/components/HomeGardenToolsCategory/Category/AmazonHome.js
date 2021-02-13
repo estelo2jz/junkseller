caf4d3075cdc7284c037e4ae16e4802c33439d5b
esteloabell <manitobaaa@gamil.com>
2021-02-12 19:12:11 -0600

    added the right styles on each component, next it to add data in component

--- src/components/HomeGardenToolsCategory/Category/AmazonHome.js
+++ src/components/HomeGardenToolsCategory/Category/AmazonHome.js
@@ -1,13 +1,21 @@
 import React from 'react';
-// import '../HomeGardenStyles/AmazonHome.scss';
+import { Link } from 'react-router-dom';
+import '../HomeGardenStyles/AmazonHome.scss';
 import ComingSoon from '../../ComingSoon';
 import Building from '../../Building'; 
 
 function AmazonHome() {
   return (
-    //  className="amazon-home__container"
-    <div>
-      <p>Home</p>
+    <div className="amazon-home__container">
+      <div className="amazon-home__history-path">
+        {/* <p>you are in :</p> */}
+        <Link to="/home&garden&tools">
+          <p>
+            /home&garden&tools
+          </p>
+        </Link>
+        <p>/home</p>
+      </div>
       <Building />
       <ComingSoon />
     </div>
