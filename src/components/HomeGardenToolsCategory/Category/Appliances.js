3349b26167e83398c5598f79a1071aae0bdb6bfc
esteloabell <manitobaaa@gamil.com>
2021-02-12 19:25:27 -0600

    styles

--- src/components/HomeGardenToolsCategory/Category/Appliances.js
+++ src/components/HomeGardenToolsCategory/Category/Appliances.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import '../HomeGardenStyles/Appliances.scss';
+import { Link } from 'react-router-dom';
+import '../HomeGardenStyles/Appliances.scss';
 import ComingSoon from '../../ComingSoon';
 import Building from '../../Building'; 
 
 function Appliances() {
   return (
     <div className="appliances__container">
-      <p>Appliances</p>
+      <div className="appliances__history-path">
+        {/* <p>you are in :</p> */}
+        <Link to="/home&garden&tools">
+          <p>
+            /home&garden&tools
+          </p>
+        </Link>
+        <p>/appliances</p>
+      </div>
       <Building />
       <ComingSoon />
     </div>
--- src/components/HomeGardenToolsCategory/Category/ArtsCrafts.js
+++ src/components/HomeGardenToolsCategory/Category/ArtsCrafts.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import '../HomeGardenStyles/ArtsCrafts.scss';
+import { Link } from 'react-router-dom';
+import '../HomeGardenStyles/ArtsCrafts.scss';
 import ComingSoon from '../../ComingSoon';
 import Building from '../../Building'; 
 
 function ArtsCrafts() {
   return (
     <div className="arts-crafts__container">
-      <p>Arts Crafts & Sewing</p>
+      <div className="arts-crafts__history-path">
+        {/* <p>you are in :</p> */}
+        <Link to="/home&garden&tools">
+          <p>
+            /home&garden&tools
+          </p>
+        </Link>
+        <p>/artscrafts&sewing</p>
+      </div>
       <Building />
       <ComingSoon />
     </div>
