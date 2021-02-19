3cfeee04eeb0eb89984a9ce96ad252ccfbff407f
esteloabell <manitobaaa@gamil.com>
2021-02-18 19:36:54 -0600

    styles

--- src/components/HandmadeCategory/Category/ClothingShoes.js
+++ src/components/HandmadeCategory/Category/ClothingShoes.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { uuid } from 'uuidv4';
 import { Link } from 'react-router-dom';
 import '../HandmadeStyles/ClothingShoes.scss';
+import HandmadeProductTemplate from './HandmadeProductTemplate';
+
 
 function ClothingShoes() {
   return (
@@ -15,6 +18,13 @@ function ClothingShoes() {
         <p>/clothing&shoes</p>
       </div>
       <div className="clothing-shoes__section-outer">
+        <div className="clothing-shoes__heading">
+          <p>The Wedding Shop</p>
+          <p>Unique, handcrafted home decor, artwork, kitchen accessories & more.</p>
+        </div>
+        <div className="clothing-shoes__header">
+          <p>Shop by category</p>
+        </div>
         <div className="clothing-shoes__section-category">
           <div className="clothing-shoes__section-links">
             <Link to="/handmade/clothing&shoes">
@@ -53,6 +63,165 @@ function ClothingShoes() {
             <p>Boys</p>
           </div>
         </div>
+      </div>
+      <div className="clothing-shoes__header">
+        <p>Collections we love</p>
+      </div>
+      <div className="clothing-shoes__products">
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
+        <HandmadeProductTemplate
+          id={uuid()}
+          image={"https://images-na.ssl-images-amazon.com/images/I/41cTvKu1plL.jpg"}
+          title="Stylish Cotton Face Mask with Filter Pocket, Handmade Floral Plaid design facemasks for women"
+          brandName="HOPE LOVE SHINE"
+          rating={4}
+          rateNumber={"792"}
+          shipping={"Free Shipping"}
+          price={"13.99"}
+          arrivalEst={"Thursday, Feb 18"}
+        />
       </div>
     </div>
   )
